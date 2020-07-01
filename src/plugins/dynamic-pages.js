b481a47d3cbd10b0a5a2acb296f094974541303d
zipang <christophe.desguez@gmail.com>
2020-07-01 11:00:57 +0200

    A plugin to defer pages rendering to template components

---
+++ src/plugins/dynamic-pages.js
@@ -0,0 +1,20 @@
+/**
+ * Docusaurus Dynamic Pages
+ * This Docusaurus plugin renders pages through deferring to template components
+ * Pages metadata must therefore include the `template` field to specify which template should be used
+ * Or else the rendering will be deferred to a default template
+ */
+export default function(conf) {
+
+    /**
+     * Template initialization
+     */
+
+    /**
+     * Plugin object with dedicated lifecycle methods
+     */
+    return {
+
+    }
+
+}
