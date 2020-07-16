e01b5dfa20f6108b3964429577b15c6da67cab18
zipang <christophe.desguez@gmail.com>
2020-07-16 13:17:33 +0200

    Working on the pages templates plugin to freely render pages

--- src/plugins/dynamic-pages.js
+++ src/plugins/dynamic-pages.js
@@ -1,20 +1,51 @@
+import path from "path";
+
+const _DEFAULT_OPTIONS = {
+	contentDirs: ["content"], // where local text content is stored in markdown + front matter format
+	templateDirs: ["src/templates", "src/sections", "src/components"]
+};
+
 /**
- * Docusaurus Dynamic Pages
+ * Docusaurus Pages Templates Plugin
  * This Docusaurus plugin renders pages through deferring to template components
  * Pages metadata must therefore include the `template` field to specify which template should be used
  * Or else the rendering will be deferred to a default template
  */
-export default function(conf) {
+const dynamicPagesPlugin = (context, opts = {}) => {
+	/**
+	 * Extract site informations and plugin options
+	 */
+	const { siteDir } = context;
+	const pluginOptions = { ..._DEFAULT_OPTIONS, ...opts };
+	const { contentDirs, templateDirs } = pluginOptions;
 
-    /**
-     * Template initialization
-     */
+	/**
+	 * Reload when a file in these paths changes
+	 */
+	const getPathsToWatch = () => [
+		...contentDirs.map((p) => path.resolve(siteDir, p, "**/*.md")),
+		...templateDirs.map((p) => path.resolve(siteDir, p, "**/*.js"))
+	];
 
-    /**
-     * Plugin object with dedicated lifecycle methods
-     */
-    return {
+	/**
+	 * Load mardown content
+	 */
+	const loadContent = () => {};
 
-    }
+	/**
+	 * Create pages for each rendered markdown file
+	 */
+	const contentLoaded = () => {};
 
-}
+	/**
+	 * Plugin object with dedicated lifecycle methods
+	 */
+	return {
+		name: "docusaurus-pages-templates-plugin",
+		getPathsToWatch,
+		loadContent,
+		contentLoaded
+	};
+};
+
+export default dynamicPagesPlugin;
