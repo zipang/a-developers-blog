82c321d6712b0742461079c7df5aa221178a9fdf
zipang <christophe.desguez@gmail.com>
2021-03-02 16:02:32 +0100

    WIP : new free pages content plugin for Docusaurus

--- src/plugins/dynamic-pages.js
+++ src/plugins/dynamic-pages.js
@@ -1,10 +1,14 @@
-import path from "path";
-import globby from "globby";
-import { parseMarkdownFile, normalizeUrl, aliasedSitePath } from "@docusaurus/utils";
+const path = require( "path");
+const globby = require( "globby");
+const react = require("react");
+const { parseMarkdownFile, fileToPath, aliasedSitePath }  = require("@docusaurus/utils");
 
 const _DEFAULT_OPTIONS = {
 	contentDirs: ["content"], // where local text content is stored in markdown + front matter format
-	templateDirs: ["src/templates", "src/sections", "src/components"]
+	defaultLayout: "_blank",
+	layouts: {
+		"_blank": () => "BLANK TEMPLATE"
+	}
 };
 
 /**
@@ -12,51 +16,87 @@ const _DEFAULT_OPTIONS = {
  * This Docusaurus plugin renders pages through deferring to template components
  * Pages metadata must therefore include the `template` field to specify which template should be used
  * Or else the rendering will be deferred to a default template
+ * @returns 
  */
 const dynamicPagesPlugin = (context, opts = {}) => {
 	/**
 	 * Extract site informations and plugin options
 	 */
 	const { siteDir } = context;
-	const pluginOptions = { ..._DEFAULT_OPTIONS, ...opts };
-	const { contentDirs, templateDirs } = pluginOptions;
+	const { contentDirs, defaultLayout, layouts } = { ..._DEFAULT_OPTIONS, ...opts };
 
 	/**
-	 * Reload when a file if these paths changes
+	 * Reload when a markdown file if these paths changes
 	 */
-	const getPathsToWatch = () => [
-		...contentDirs.map((p) => path.resolve(siteDir, p, "**/*.md")),
-		...templateDirs.map((p) => path.resolve(siteDir, p, "**/*.js"))
-	];
+	const getPathsToWatch = () => contentDirs.map((p) => path.resolve(siteDir, p, "**/*.md"));
 
 	/**
 	 * Load files content (markdown+front-matter)
+	 * @returns Promise<Array<ParsedMarkdown>>
 	 */
 	const loadContent = async () => {
-		// Get the matching files
-		const files = await globby(include, {
-			cwd: siteDir
+		// Get the matching files full paths
+		const files = await globby(contentDirs, {
+			expandDirectories: {
+				extensions: ["md"]
+			},
+			cwd: siteDir,
+			gitignore: true
 		});
 
-		files.map((fullpath) => {
-			const parsedContent = parseMarkdownFile(fullpath);
-		});
+		const parsedFiles = await Promise.all(
+			files.map(async (fullpath) => {
+				const { frontMatter, content, excerpt } = await parseMarkdownFile(
+					fullpath
+				);
+				const metadata = {
+					path: fileToPath(fullpath),
+				}
+				return {
+					data: frontMatter,
+					metadata,
+					content,
+					excerpt
+				};
+			})
+		);
+
+		return parsedFiles;
 	};
 
 	/**
 	 * Create pages for each rendered markdown file
 	 */
-	const contentLoaded = async () => {};
+	const contentLoaded = async ({ content, actions }) => {
+		const { addRoute } = actions;
+		await Promise.all(
+			content.map(async ({metadata}) => {
+				const { layout } = metadata;
+				// Find the layout component
+				const layoutCmpnt = layouts[layout] || layouts[defaultLayout];
+
+				if (typeof layoutCmpnt !== "function") {
+					throw new TypeError(`Layout ${layout} isn't a valid React component. Cannot render page ${path}`)
+				}
+
+				addRoute({
+					path: metadata.path,
+					component: layoutCmpnt,
+					exact: true
+				});
+			})
+		);
+	}
 
 	/**
 	 * Plugin object with dedicated lifecycle methods
 	 */
 	return {
-		name: "docusaurus-pages-templates-plugin",
+		name: "dynamic-pages-plugin",
 		getPathsToWatch,
 		loadContent,
 		contentLoaded
 	};
 };
 
-export default dynamicPagesPlugin;
+module.exports = dynamicPagesPlugin;
--- src/plugins/dynamic-pages.specs.js
+++ src/plugins/dynamic-pages.specs.js
@@ -1,5 +1,7 @@
 import dynamicPagesPlugin from "./dynamic-pages";
 
+const {loadContent} = dynamicPagesPlugin;
+
 /**
  * Test suite for the lifecycle API exposed by the dynamic pages plugin
  */
